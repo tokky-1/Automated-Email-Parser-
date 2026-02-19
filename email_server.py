fcf675ecfd9b24d362d196a74acace4701e526f6
Ayo-Ajayi Oluwatokiloba <tokkyayoajayi@gmail.com>
2026-02-19 17:25:34 +0100

    feat:add google oauth2 login and structures the raw data

--- email_server.py
+++ email_server.py
@@ -1,2 +1,60 @@
 # API integration
-#Purpose: Handles connection to email (IMAP/SMTP or API)
+#Purpose: Handles connection to email (gmail API)
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+import os
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
+#gets and saves user credentials
+def authenticate():
+    creds = None
+    #checks for saved tokens
+    if os.path.exists("token.json"): 
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # checks for none or if it has expired
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request()) # refreshes
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0) # opens the browser login
+
+        with open("token.json", "w") as token:
+            token.write(creds.to_json()) # saves the credentials
+
+    return creds
+
+def fetch_emails():
+    creds = authenticate()
+    #creates api client
+    service = build("gmail", "v1", credentials=creds)
+
+    # get the ids of the first 150 emails
+    results = service.users().messages().list(
+        userId="me",
+        maxResults=150
+    ).execute()
+    messages = results.get("messages", [])
+    if not messages:
+        print("No emails found.")
+        return []
+
+    #prints out the emails in details
+    emails = []
+    for msg in messages:
+        detail = service.users().messages().get(
+            userId="me",
+            id=msg["id"],
+            format="full"
+        ).execute()
+        emails.append(detail)
+
+    return emails
+#for test purposes only
+# if __name__ == "__main__":
+#     emails = fetch_emails()
+#     print(f"Fetched {len(emails)} emails")
+#     print(emails[0])  # prints the first raw email so you can see the structure
--- parser.py
+++ parser.py
@@ -1,2 +1,35 @@
 #Data structuring
 #Purpose: Extracts useful info from raw email data
+import base64
+
+def parse_email(raw_email):
+    #holds the main content of the email
+    payload = raw_email["payload"]
+
+    #holds sender,reciever,date
+    headers = payload["headers"]
+    subject = next((h["value"] for h in headers if h["name"] == "Subject"), "No Subject")
+    sender  = next((h["value"] for h in headers if h["name"] == "From"), "Unknown")
+    date    = next((h["value"] for h in headers if h["name"] == "Date"), "Unknown")
+
+    #converts the text version gotten from google to plain text
+    body:str = ""
+    parts = payload.get("parts", [])
+    for part in parts:
+        if part["mimeType"] == "text/plain":
+            data = part["body"]["data"]
+            body = base64.urlsafe_b64decode(data).decode("utf-8")
+            break
+    return {
+        "subject": subject,
+        "sender":  sender,
+        "date":    date,
+        "body":    body,
+    }
+
+if __name__ == "__main__":
+    from email_server import fetch_emails
+    
+    raw_emails = fetch_emails()
+    parsed = parse_email(raw_emails[0])
+    print(parsed)
